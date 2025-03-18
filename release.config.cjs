cb30acefc1d5352e9902f74a891253d5a7ae20a8
Nazmuz Pranto <npranto@gmail.com>
2025-03-18 18:07:15 -0400

    feat: #6 - adds gh action for healthcheck and versioning with semantic release

---
+++ release.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            type: 'minor',
+            release: 'minor',
+          },
+          { type: 'major', release: 'major' },
+          { release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message: '🚀 v${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
+}
