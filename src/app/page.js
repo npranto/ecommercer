a70e7475dc071b7d339d5f805eb335b9869bcb4a
Nazmuz Pranto <npranto@gmail.com>
2025-03-19 02:08:27 -0400

    feat: #9 - updates bg color to red

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen max-w-screen-lg mx-auto bg-gray-100">
+    <div className="flex flex-col min-h-screen max-w-screen-lg mx-auto bg-red-100">
       <header className="header">Header</header>
       <main className="flex flex-col">
         <h1>Hello ecommercer!</h1>
