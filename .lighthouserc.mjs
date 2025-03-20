39cee148c906cb3d762525e1594f27b034fe0aaf
Nazmuz Pranto <npranto@gmail.com>
2025-03-20 15:57:20 -0400

    feat: #13 - adds deployment url on PR

---
+++ .lighthouserc.mjs
@@ -0,0 +1,22 @@
+export default {
+	ci: {
+		assert: {
+			assertions: {
+				// Performance should be above 90%
+				'categories:performance': ['error', { minScore: 0.9 }],
+				// LCP should be <= 2500ms
+				'audits:largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
+				// FID should be <= 100ms
+				'audits:first-input-delay': ['error', { maxNumericValue: 100 }],
+				// CLS should be <= 0.1
+				'audits:cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
+				// FCP should be <= 2000ms
+				'audits:first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
+				// TTI should be <= 5000ms
+				'audits:interactive': ['warn', { maxNumericValue: 5000 }],
+				// TBT should be <= 300ms
+				'audits:total-blocking-time': ['warn', { maxNumericValue: 300 }],
+			},
+		},
+	},
+};
---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+	return (
+		<div className="flex flex-col min-h-screen max-w-screen-lg mx-auto bg-yellow-100">
+			<header className="header">About Header</header>
+			<main className="flex flex-col">
+				<h1>About Page</h1>
+			</main>
+			<footer className="flex flex-col">About Footer</footer>
+		</div>
+	);
+}
